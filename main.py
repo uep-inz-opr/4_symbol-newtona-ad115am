a292399c4c31227bcc73372cc88be5e0a243713c
ad115am <twente333@gmail.com>
2022-05-03 13:38:48 +0200

    wersja bez funkcji

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import math
 
-def newton():
-    numbers = list(map(int, input().split()))
-    n = numbers[0]
-    k = numbers[1]
-    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
-print(newton())
+numbers = list(map(int, input().split()))
+n = numbers[0]
+k = numbers[1]
+result= math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+print(result)
